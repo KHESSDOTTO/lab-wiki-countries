9d0b4f101232f3495a1b999c1966ce1fbed16944
Khess S Dotto <khessdotto@gmail.com>
2023-02-05 22:03:34 -0300

    NavBar prettier ('bg-primary' instead of 'bg-secondary')

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function NavBar () {
   return (
-    <nav className="navbar navbar-dark bg-secondary">
+    <nav className="navbar navbar-dark bg-primary">
       <div className="container">
         <Link className="navbar-brand"  to="/">WikiCountries</Link>
       </div>
